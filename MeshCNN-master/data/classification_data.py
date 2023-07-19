69938b8f986aae735aa02fe943f98ebc502c110a
sumali-90 <sumali.nuwanthika90@gmail.com>
2023-07-19 00:18:15 -0400

    Commiting the chages done in classification_data file

--- MeshCNN-master/data/classification_data.py
+++ MeshCNN-master/data/classification_data.py
@@ -4,18 +4,22 @@ from data.base_dataset import BaseDataset
 from util.util import is_mesh_file, pad
 from models.layers.mesh import Mesh
 
+# Dataset for clasification.
 class ClassificationData(BaseDataset):
 
     def __init__(self, opt):
         BaseDataset.__init__(self, opt)
         self.opt = opt
         self.device = torch.device('cuda:{}'.format(opt.gpu_ids[0])) if opt.gpu_ids else torch.device('cpu')
+        # Sets the folder name and the classes based on the folders
         self.root = opt.dataroot
         self.dir = os.path.join(opt.dataroot)
         self.classes, self.class_to_idx = self.find_classes(self.dir)
+        # Extracts the paths of each mesh and its labels based on the paths
         self.paths = self.make_dataset_by_class(self.dir, self.class_to_idx, opt.phase)
         self.nclasses = len(self.classes)
         self.size = len(self.paths)
+        # Get mean and std of the edge features
         self.get_mean_std()
         # modify for network later.
         opt.nclasses = self.nclasses
@@ -26,10 +30,11 @@ class ClassificationData(BaseDataset):
         label = self.paths[index][1]
         mesh = Mesh(file=path, opt=self.opt, hold_history=False, export_folder=self.opt.export_folder)
         meta = {'mesh': mesh, 'label': label}
-        # get edge features
-        edge_features = mesh.extract_features()
-        edge_features = pad(edge_features, self.opt.ninput_edges)
-        meta['edge_features'] = (edge_features - self.mean) / self.std
+
+        # get edge or face features
+        features = mesh.extract_features()
+        features = pad(features, self.opt.ninput_features)
+        meta['features'] = (features - self.mean) / self.std
         return meta
 
     def __len__(self):
